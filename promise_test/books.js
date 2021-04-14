a5d72b105eb95328d4d211620c9967c1f7496ba1
thanh <tnguyen1138@gmail.com>
2021-04-14 08:16:16 -0700

    promise_test

---
+++ promise_test/books.js
@@ -0,0 +1,23 @@
+﻿function booksinfo() {
+    let p = fetch("https://fakerestapi.azurewebsites.net/api/v1/books").then(function (response) {
+        return response.json()
+    })
+
+    p.then(function (data) {
+        console.log(data);
+        let tbody = document.querySelector('tbody');
+        tbody.innerHTML = "";
+        let length = data.length;
+        for (let i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td> "
+                + data[i].title + "</td> <td>"
+                + data[i].description + "</td> <td>"
+                + data[i].pageCount + "</td> <td>"
+                + data[i].excerpt + "</td> <td>"
+                + data[i].publishDate + "</td>  </tr > ";
+            tbody.innerHTML += tr;
+        }
+    }).catch(function (e) {
+
+    })
+}
---
+++ promise_test/user.js
@@ -0,0 +1,18 @@
+﻿function usersinfo() {
+    let p = fetch("https://fakerestapi.azurewebsites.net/api/v1/Users").then(function (response) {
+        return response.json()
+    })
+
+    p.then(function (data) {
+        console.log(data);
+        let tbody = document.querySelector('tbody');
+        tbody.innerHTML = "";
+        let length = data.length;
+        for (let i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td>" + data[i].userName + "</td> <td>" + data[i].password + "</td> </tr>";
+            tbody.innerHTML += tr;
+        }
+    }).catch(function (e) {
+
+    })
+}
